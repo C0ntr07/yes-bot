8569e6f326525520fdc228ed71b54aeafb28c731
Max Kung <maxkung101@users.noreply.github.com>
2018-04-03 07:22:55 -0400

    Update yesbot.py
    Increased font size, good for touchscreens

--- yesbot.py
+++ yesbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from tkinter import *
+from tkinter.font import Font
 import pygame.mixer
 
 def sayYes():
@@ -7,7 +8,7 @@ def sayYes():
 
 app = Tk()
 app.title("Yes Bot")
-app.geometry('350x100+200+100')
+app.geometry('450x120+200+100')
 app.resizable(False, False)
 sounds = pygame.mixer
 sounds.init()
@@ -16,12 +17,14 @@ yes_en = sounds.Sound("wav/c-falcon_YES.wav") # You can replace this sound with 
                                               # For French speaking telemarketers, say "oui."
                                               # For Spanish speaking telemarketers, say "si."
 
-lab = Label(app, text='Annoy those telemarketers with one word.', height = 3) # French: Agiter les télévendeurs avec un mot.
-                                                                              # Spanish: Molestar a los agentes de telemercadeo con una sola palabra.
+myFont = Font(family="Arial", size = 12)
+
+lab = Label(app, text='Annoy those telemarketers with one word.', height = 3, font = myFont) # French: Agiter les télévendeurs avec un mot.
+                                                                                             # Spanish: Molestar a los agentes de telemercadeo con una sola palabra.
 lab.pack(side = TOP)
 
-btn = Button(app, text = "\"Yes\"", width = 10, command = sayYes) # French: Oui
-                                                                  # Spanish: Si
+btn = Button(app, text = "\"Yes\"", width = 10, command = sayYes, font = myFont) # French: Oui
+                                                                                 # Spanish: Si
 btn.pack(side = TOP)
 
 app.mainloop()
