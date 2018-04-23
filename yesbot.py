b3231578ae52418580460f274fabe3b513701f4c
Max Kung <maxkung101@users.noreply.github.com>
2018-04-23 09:34:04 -0400

    Update yesbot.py

--- yesbot.py
+++ yesbot.py
@@ -20,17 +20,12 @@ sounds.init()
 yes_en = sounds.Sound("wav/c-falcon_YES.wav") # You can replace this sound with any other voice saying "yes." Be sure to replace the "c-falcon_YES.wav" in this line with the new recorded sound file that you saved in the wav folder.
                                               # For French speaking telemarketers, say "oui."
                                               # For Spanish speaking telemarketers, say "si."
-
 myFont = Font(family = "Arial", size = 12)
-
-lab = Label(app, text = 'Annoy those telemarketers with one word.', height = 3, font = myFont) # French: Agiter les télévendeurs avec un mot.
-                                                                                             # Spanish: Molestar a los agentes de telemercadeo con una sola palabra.
+lab = Label(app, text = 'Annoy those telemarketers with one word.', # French: Agiter les télévendeurs avec un mot.
+            height = 3, font = myFont)                              # Spanish: Molestar a los agentes de telemercadeo con una sola palabra.
 lab.pack(side = TOP)
-
-btn = Button(app, text = "\"Yes\"", width = 10, command = sayYes, font = myFont) # French: Oui
-                                                                                 # Spanish: Si
+btn = Button(app, text = "\"Yes\"",                       # French: Oui
+             width = 10, command = sayYes, font = myFont) # Spanish: Si
 btn.pack(side = TOP)
-
 app.protocol("WM_DELETE_WINDOW", shutdown)
-
 app.mainloop()
