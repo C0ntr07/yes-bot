83ce671e2ae13477d51261f08bf726dbd7c49a56
Max Kung <maxkung101@users.noreply.github.com>
2018-04-25 18:19:11 -0400

    Update yesbot.py

--- yesbot.py
+++ yesbot.py
@@ -4,7 +4,7 @@ from tkinter.font import Font
 import pygame.mixer
 
 def sayYes():
-    yes_en.play()
+    yes_s.play()
 
 def shutdown():
     sounds.stop()
@@ -14,13 +14,14 @@ app = Tk()
 app.title("Yes Bot")
 app.geometry('450x120+200+100')
 app.resizable(False, False)
+myFont = Font(family = "Arial", size = 12)
 sounds = pygame.mixer
 sounds.init()
-
-yes_en = sounds.Sound("wav/c-falcon_YES.wav") # You can replace this sound with any other voice saying "yes." Be sure to replace the "c-falcon_YES.wav" in this line with the new recorded sound file that you saved in the wav folder.
-                                              # For French speaking telemarketers, say "oui."
-                                              # For Spanish speaking telemarketers, say "si."
-myFont = Font(family = "Arial", size = 12)
+yes_s = sounds.Sound("wav/c-falcon_YES.wav") # You can replace this sound with any other voice saying "yes." Be sure to replace the
+                                             # "c-falcon_YES.wav" in this line with the new recorded sound file that you saved in the
+                                             # wav folder.
+                                             # For French speaking telemarketers, say "oui."
+                                             # For Spanish speaking telemarketers, say "si."
 lab = Label(app, text = 'Annoy those telemarketers with one word.', # French: Agiter les télévendeurs avec un mot.
             height = 3, font = myFont)                              # Spanish: Molestar a los agentes de telemercadeo con una sola palabra.
 lab.pack(side = TOP)
