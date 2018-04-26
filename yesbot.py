68fd362d4edd0f4a4a66ba8d3264d373ea7f3f13
Max Kung <maxkung101@users.noreply.github.com>
2018-04-26 08:38:31 -0400

    Update yesbot.py

--- yesbot.py
+++ yesbot.py
@@ -17,16 +17,31 @@ app.resizable(False, False)
 myFont = Font(family = "Arial", size = 12)
 sounds = pygame.mixer
 sounds.init()
-yes_s = sounds.Sound("wav/c-falcon_YES.wav") # You can replace this sound with any other voice saying "yes." Be sure to replace the
-                                             # "c-falcon_YES.wav" in this line with the new recorded sound file that you saved in the
-                                             # wav folder.
-                                             # For French speaking telemarketers, say "oui."
-                                             # For Spanish speaking telemarketers, say "si."
-lab = Label(app, text = 'Annoy those telemarketers with one word.', # French: Agiter les télévendeurs avec un mot.
-            height = 3, font = myFont)                              # Spanish: Molestar a los agentes de telemercadeo con una sola palabra.
+yes_s = sounds.Sound("wav/c-falcon_YES.wav")
+#                    You can replace this sound with any other voice saying
+#                    "yes." Be sure to replace the "c-falcon_YES.wav" in this
+#                    line with the new recorded sound file that you saved in
+#                    the wav folder.
+#                    For French speaking telemarketers, say "oui."
+#                    For Spanish speaking telemarketers, say "si."
+lab = Label(
+    app,
+    text = 'Annoy those telemarketers with one word.',
+#          French: Agiter les télévendeurs avec un mot.
+#          Spanish: Molestar a los agentes de telemercadeo con una sola palabra.
+    height = 3,
+    font = myFont
+)
 lab.pack(side = TOP)
-btn = Button(app, text = "\"Yes\"",                       # French: Oui
-             width = 10, command = sayYes, font = myFont) # Spanish: Si
+btn = Button(
+    app,
+    text = "\"Yes\"",
+#          French: Oui
+#          Spanish: Si
+    width = 10,
+    command = sayYes,
+    font = myFont
+)
 btn.pack(side = TOP)
 app.protocol("WM_DELETE_WINDOW", shutdown)
 app.mainloop()
